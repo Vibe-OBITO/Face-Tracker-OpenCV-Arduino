68773b1441eeb8a0a6e4c38d6e29236ecc0920c8
OBITO <90102005+Vibe-OBITO@users.noreply.github.com>
2023-07-03 15:06:59 +0300

    Update python_script.py

--- python_script.py
+++ python_script.py
@@ -1,44 +1,54 @@
-#Face tracker using OpenCV and Arduino
-#by Shubham Santosh
-
 import cv2
-import serial,time
-face_cascade= cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-cap=cv2.VideoCapture(1)
-#fourcc= cv2.VideoWriter_fourcc(*'XVID')
-ArduinoSerial=serial.Serial('com7',9600,timeout=0.1)
-#out= cv2.VideoWriter('face detection4.avi',fourcc,20.0,(640,480))
+import serial
+import time
+
+# Загрузка каскада Хаара для обнаружения лиц
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+# Инициализация видеозахвата с камеры
+cap = cv2.VideoCapture(1)
+
+# Инициализация соединения с Arduino по последовательному порту
+ArduinoSerial = serial.Serial('com7', 9600, timeout=0.1)
+
+# Задержка для установления связи с Arduino
 time.sleep(1)
 
 while cap.isOpened():
-    ret, frame= cap.read()
-    frame=cv2.flip(frame,1)  #mirror the image
-    #print(frame.shape)
-    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-    faces= face_cascade.detectMultiScale(gray,1.1,6)  #detect the face
-    for x,y,w,h in faces:
-        #sending coordinates to Arduino
-        string='X{0:d}Y{1:d}'.format((x+w//2),(y+h//2))
+    # Считывание кадра с камеры
+    ret, frame = cap.read()
+
+    # Отражение изображения по горизонтали (зеркальное отображение)
+    frame = cv2.flip(frame, 1)
+
+    # Преобразование изображения в оттенки серого
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Обнаружение лиц на кадре
+    faces = face_cascade.detectMultiScale(gray, 1.1, 6)
+
+    for x, y, w, h in faces:
+        # Отправка координат лица в Arduino
+        string = 'X{0:d}Y{1:d}'.format((x + w // 2), (y + h // 2))
         print(string)
         ArduinoSerial.write(string.encode('utf-8'))
-        #plot the center of the face
-        cv2.circle(frame,(x+w//2,y+h//2),2,(0,255,0),2)
-        #plot the roi
-        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),3)
-    #plot the squared region in the center of the screen
-    cv2.rectangle(frame,(640//2-30,480//2-30),
-                 (640//2+30,480//2+30),
-                  (255,255,255),3)
-    #out.write(frame)
-    cv2.imshow('img',frame)
-    #cv2.imwrite('output_img.jpg',frame)
-    '''for testing purpose
-    read= str(ArduinoSerial.readline(ArduinoSerial.inWaiting()))
-    time.sleep(0.05)
-    print('data from arduino:'+read)
-    '''
-    # press q to Quit
-    if cv2.waitKey(10)&0xFF== ord('q'):
+
+        # Отрисовка центра лица
+        cv2.circle(frame, (x + w // 2, y + h // 2), 2, (0, 255, 0), 2)
+
+        # Отрисовка прямоугольника вокруг лица
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 3)
+
+    # Отрисовка прямоугольника в центре экрана
+    cv2.rectangle(frame, (640 // 2 - 30, 480 // 2 - 30), (640 // 2 + 30, 480 // 2 + 30), (255, 255, 255), 3)
+
+    # Отображение кадра
+    cv2.imshow('img', frame)
+
+    # Нажмите 'q' для выхода
+    if cv2.waitKey(10) & 0xFF == ord('q'):
         break
+
+# Освобождение ресурсов
 cap.release()
 cv2.destroyAllWindows()
